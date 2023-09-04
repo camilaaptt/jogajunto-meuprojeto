92e39c90c6c11c4ac35bbf64da3b6b41385dd917
Camila Teixeira <camila.teixeira@visto.global>
2023-09-04 18:06:58 -0300

    subindo aula e atividades

---
+++ A4M4-aula.py
@@ -0,0 +1,20 @@
+'''
+Operadores e collections
+
+Adição -> +
+Subtração -> -
+Divisão -> /
+Multiplicação -> *
+Potência -> **
+Resto da divisão -> %
+Divisão inteira -> //
+
+regra de três:
+(valorProduto * (percentualDesconto/100))
+
+
+'''
+# n = 9**(1/2)
+n = (589 * (25/100))
+n1 = 589 - n
+print(n1)
