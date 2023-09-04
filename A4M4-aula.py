a689939c0b61c051fefe901242ff4a8b876b7a46
Camila Teixeira <camila.teixeira@visto.global>
2023-09-04 19:41:29 -0300

    adicionando mais uma atividade

--- A4M4-aula.py
+++ A4M4-aula.py
@@ -12,9 +12,18 @@ Divisão inteira -> //
 regra de três:
 (valorProduto * (percentualDesconto/100))
 
+Operadores de atribuição
+
+= -> x = 1
++= -> x + 1
+-= -> x - 1
+*= -> x * 1
+/= ->
+%= -> x % 1
 
 '''
 # n = 9**(1/2)
-n = (589 * (25/100))
-n1 = 589 - n
-print(n1)
+# n = (589 * (25/100))
+# n1 = 589 - n
+# print(n1)
+
