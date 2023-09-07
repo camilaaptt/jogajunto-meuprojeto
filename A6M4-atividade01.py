d750a5588bb7d65fd90b09252ecc71388331193a
Camila Teixeira <camila.teixeira@visto.global>
2023-09-07 01:40:24 -0300

    adicionando desafios e atividade para casa

---
+++ A6M4-atividade01.py
@@ -0,0 +1,20 @@
+'''
+AULA:
+
+for i in range(1, 11):
+    print(i)
+
+ATIVIDADE:
+
+PARTE 1
+
+INDIVIDUAL
+
+Faça um for e imprima na tela todos os numeros de 1 até 1000. 
+Depois, crie uma estrutura condicional para descobrir e printar apenas os números que forem par.      
+   
+'''
+
+for i in range(1, 1001):
+    if i % 2 == 0:
+        print(i)
---
+++ A6M4-atividade04.py
@@ -0,0 +1,27 @@
+'''
+AULA:
+
+count = 1 
+
+while count <= 10:
+    print(count)
+    count += 1
+
+ATIVIDADE:
+
+DESAFIO DO CAIQUE
+
+Individualmente - Acompanhando os facilitadores
+
+Use um loop "while" em Python para imprimir a mensagem enquanto o valor de x não for igual a 5. 
+
+Aproveite para tirar todas as suas dúvidas.    
+   
+'''
+
+x = 0
+
+while x < 5:
+    print(f'O valor de X é {x} e está abaixo de 5')
+    x += 1
+print("Fim! Agora o valor de X é 5")
